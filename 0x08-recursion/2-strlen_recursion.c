31cb70bc052dc9c878fa3aa7766a25425a0be1f3
Hanonray <hanonraychristian@gmail.com>
2023-11-01 10:13:59 -0700

    printing the length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - to find the length of a string
+ * @s: the string to be calculated size
+ * Return: the length of s
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	s++;
+	return(_strlen_recursion(s) + 1);
+}
